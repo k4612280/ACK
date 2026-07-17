e67bbb3cfdde6c129c4769e5667844801fb8100c
k4612280 <k4612280@gmail.com>
2026-07-17 14:37:13 +0300

    Update officers.js

--- src/database/officers.js
+++ src/database/officers.js
@@ -1,1 +1,28 @@
-ايفا
+const db = require("./init");
+
+module.exports = {
+
+    create(data) {
+
+        return db.prepare(`
+        INSERT INTO officers
+        (guild_id,user_id,fingerprint,rank_name)
+        VALUES (?,?,?,?)
+        `).run(
+            data.guild_id,
+            data.user_id,
+            data.fingerprint,
+            data.rank_name
+        );
+
+    },
+
+    get(userId) {
+
+        return db.prepare(
+            "SELECT * FROM officers WHERE user_id=?"
+        ).get(userId);
+
+    }
+
+};
