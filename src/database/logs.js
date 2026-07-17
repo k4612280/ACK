500454ab7ff943829ab71d2064d32378634ca17b
k4612280 <k4612280@gmail.com>
2026-07-17 14:37:31 +0300

    Update logs.js

--- src/database/logs.js
+++ src/database/logs.js
@@ -1,1 +1,20 @@
-حمود وبلتي
+const db = require("./init");
+
+module.exports = {
+
+    create(action,userId,executorId,guildId){
+
+        db.prepare(`
+        INSERT INTO logs
+        (guild_id,action,user_id,executor_id)
+        VALUES(?,?,?,?)
+        `).run(
+            guildId,
+            action,
+            userId,
+            executorId
+        );
+
+    }
+
+};
