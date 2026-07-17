5e02ff5c07c26da1834d25acd688958d4883bae2
k4612280 <k4612280@gmail.com>
2026-07-17 14:26:11 +0300

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,1 +1,24 @@
-h
+const fs = require("fs");
+const path = require("path");
+
+module.exports = async (client) => {
+
+    const commandsPath = path.join(__dirname, "../commands");
+
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+
+        const command = require(path.join(commandsPath, file));
+
+        if (command.data && command.execute) {
+
+            client.commands.set(command.data.name, command);
+
+            console.log(`✅ Loaded Command: ${command.data.name}`);
+
+        }
+
+    }
+
+};
