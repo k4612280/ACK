0d6470118b1f39905a4ddd0d02686a93f9476de5
k4612280 <k4612280@gmail.com>
2026-07-17 14:36:28 +0300

    Update settings.js

--- src/database/settings.js
+++ src/database/settings.js
@@ -1,1 +1,23 @@
-د
+const db = require("./init");
+
+module.exports = {
+
+    get(guildId) {
+
+        return db.prepare("SELECT * FROM settings WHERE guild_id = ?").get(guildId);
+
+    },
+
+    create(guildId) {
+
+        return db.prepare("INSERT OR IGNORE INTO settings (guild_id) VALUES (?)").run(guildId);
+
+    },
+
+    update(column, value, guildId) {
+
+        return db.prepare(`UPDATE settings SET ${column} = ? WHERE guild_id = ?`).run(value, guildId);
+
+    }
+
+};
