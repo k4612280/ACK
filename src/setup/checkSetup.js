361af8ffe3083aba0027d7c19331a7c686a06fd6
k4612280 <k4612280@gmail.com>
2026-07-17 18:36:38 +0300

    Update checkSetup.js

--- src/setup/checkSetup.js
+++ src/setup/checkSetup.js
@@ -1,1 +1,17 @@
-ي
+const settings = require("../database/settings");
+
+module.exports = (guildId) => {
+
+    let data = settings.get(guildId);
+
+    if (!data) {
+
+        settings.create(guildId);
+
+        data = settings.get(guildId);
+
+    }
+
+    return data;
+
+};
