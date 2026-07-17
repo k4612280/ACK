e83cdfafe507b5bddc85c1ef5b955b7adf60a331
k4612280 <k4612280@gmail.com>
2026-07-17 21:10:16 +0300

    Update fingerprint.js

--- src/database/fingerprint.js
+++ src/database/fingerprint.js
@@ -1,1 +1,62 @@
-قحباتي
+const db = require("./init");
+
+module.exports = {
+
+    create(data) {
+
+        return db.prepare(`
+            INSERT INTO fingerprints (
+
+                guild_id,
+                officer_id,
+                officer_name,
+                type
+
+            )
+
+            VALUES (
+
+                @guild_id,
+                @officer_id,
+                @officer_name,
+                @type
+
+            )
+        `).run(data);
+
+    },
+
+    get(officerId) {
+
+        return db.prepare(`
+            SELECT *
+            FROM fingerprints
+            WHERE officer_id = ?
+            ORDER BY id DESC
+        `).all(officerId);
+
+    },
+
+    latest(officerId) {
+
+        return db.prepare(`
+            SELECT *
+            FROM fingerprints
+            WHERE officer_id = ?
+            ORDER BY id DESC
+            LIMIT 1
+        `).get(officerId);
+
+    },
+
+    all() {
+
+        return db.prepare(`
+            SELECT *
+            FROM fingerprints
+            ORDER BY id DESC
+        `).all();
+
+    }
+
+};
