af7adb90c100d31d0e2423ee26e2e29c6bbd57fe
k4612280 <k4612280@gmail.com>
2026-07-17 14:27:22 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,1 +1,21 @@
-ا
+module.exports = {
+
+    name: "ready",
+
+    once: true,
+
+    async execute(client) {
+
+        console.clear();
+
+        console.log("====================================");
+        console.log("Government System");
+        console.log(`Logged in as ${client.user.tag}`);
+        console.log(`Bot ID: ${client.user.id}`);
+        console.log("====================================");
+
+        client.user.setActivity("Government System");
+
+    }
+
+};
