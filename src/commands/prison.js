a958eb9cf5aff9c7162aa919cfd58b675df3351d
k4612280 <k4612280@gmail.com>
2026-07-17 21:12:08 +0300

    Update prison.js

--- src/commands/prison.js
+++ src/commands/prison.js
@@ -1,1 +1,39 @@
-ا
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits
+} = require("discord.js");
+
+const officerPanel = require("../embeds/officerPanel");
+const officersMenu = require("../menus/officersMenu");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+
+        .setName("prison")
+
+        .setDescription("لوحة نظام السجن")
+
+        .setDefaultMemberPermissions(
+            PermissionFlagsBits.Administrator
+        ),
+
+    async execute(interaction) {
+
+        await interaction.reply({
+
+            embeds: [
+                officerPanel()
+            ],
+
+            components: [
+                officersMenu()
+            ],
+
+            ephemeral: true
+
+        });
+
+    }
+
+};
