f2b9947ea7c7916df0853c81b5a8b62d49c3b04c
k4612280 <k4612280@gmail.com>
2026-07-17 18:57:08 +0300

    Update violationModal.js

--- src/modals/violationModal.js
+++ src/modals/violationModal.js
@@ -1,1 +1,33 @@
-ذ
+const {
+    ModalBuilder,
+    ActionRowBuilder,
+    TextInputBuilder,
+    TextInputStyle
+} = require("discord.js");
+
+module.exports = () => {
+
+    const modal = new ModalBuilder()
+        .setCustomId("violation_modal")
+        .setTitle("تسجيل مخالفة");
+
+    const user = new TextInputBuilder()
+        .setCustomId("user")
+        .setLabel("آيدي أو منشن الشخص")
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+
+    const reason = new TextInputBuilder()
+        .setCustomId("reason")
+        .setLabel("سبب المخالفة")
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(true);
+
+    modal.addComponents(
+        new ActionRowBuilder().addComponents(user),
+        new ActionRowBuilder().addComponents(reason)
+    );
+
+    return modal;
+
+};
