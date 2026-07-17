c84f8033c9b2168ac371de26e6a6bae554168e3e
k4612280 <k4612280@gmail.com>
2026-07-17 14:27:54 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,1 +1,43 @@
-ت
+module.exports = {
+
+    name: "interactionCreate",
+
+    async execute(interaction, client) {
+
+        if (interaction.isChatInputCommand()) {
+
+            const command = client.commands.get(interaction.commandName);
+
+            if (!command) return;
+
+            try {
+
+                await command.execute(interaction, client);
+
+            } catch (error) {
+
+                console.error(error);
+
+                if (interaction.replied || interaction.deferred) {
+
+                    await interaction.followUp({
+                        content: "حدث خطأ أثناء تنفيذ الأمر.",
+                        ephemeral: true
+                    });
+
+                } else {
+
+                    await interaction.reply({
+                        content: "حدث خطأ أثناء تنفيذ الأمر.",
+                        ephemeral: true
+                    });
+
+                }
+
+            }
+
+        }
+
+    }
+
+};
