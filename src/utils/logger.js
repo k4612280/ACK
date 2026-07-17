c8879f9670eb37a2acab68ebc7bf1871a62a18a8
k4612280 <k4612280@gmail.com>
2026-07-17 14:35:33 +0300

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,27 @@
-ام
+module.exports = {
+
+    info(message) {
+
+        console.log(`ℹ️ ${message}`);
+
+    },
+
+    success(message) {
+
+        console.log(`✅ ${message}`);
+
+    },
+
+    warn(message) {
+
+        console.log(`⚠️ ${message}`);
+
+    },
+
+    error(message) {
+
+        console.log(`❌ ${message}`);
+
+    }
+
+};
