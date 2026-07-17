726de10c8a9ed33d38eb3a9544ae1f910dfc0a0c
k4612280 <k4612280@gmail.com>
2026-07-17 14:36:49 +0300

    Update prisoners.js

--- src/database/prisoners.js
+++ src/database/prisoners.js
@@ -1,1 +1,40 @@
-ح
+const db = require("./init");
+
+module.exports = {
+
+    create(data) {
+
+        return db.prepare(`
+        INSERT INTO prisoners
+        (guild_id,user_id,officer_id,reason,sentence)
+        VALUES (?,?,?,?,?)
+        `).run(
+            data.guild_id,
+            data.user_id,
+            data.officer_id,
+            data.reason,
+            data.sentence
+        );
+
+    },
+
+    get(userId) {
+
+        return db.prepare(
+            "SELECT * FROM prisoners WHERE user_id = ? AND status='jailed'"
+        ).get(userId);
+
+    },
+
+    release(userId) {
+
+        return db.prepare(`
+        UPDATE prisoners
+        SET status='released',
+        released_at=CURRENT_TIMESTAMP
+        WHERE user_id=?
+        `).run(userId);
+
+    }
+
+};
