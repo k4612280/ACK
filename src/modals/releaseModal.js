2ad41013b7fbe1881da3d124977d33c54dacdf1c
k4612280 <k4612280@gmail.com>
2026-07-17 18:55:45 +0300

    Update releaseModal.js

--- src/modals/releaseModal.js
+++ src/modals/releaseModal.js
@@ -1,1 +1,33 @@
-ط
+const {
+    ModalBuilder,
+    ActionRowBuilder,
+    TextInputBuilder,
+    TextInputStyle
+} = require("discord.js");
+
+module.exports = () => {
+
+    const modal = new ModalBuilder()
+        .setCustomId("release_modal")
+        .setTitle("إطلاق سراح سجين");
+
+    const user = new TextInputBuilder()
+        .setCustomId("user")
+        .setLabel("آيدي أو منشن السجين")
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+
+    const reason = new TextInputBuilder()
+        .setCustomId("reason")
+        .setLabel("سبب إطلاق السراح")
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(true);
+
+    modal.addComponents(
+        new ActionRowBuilder().addComponents(user),
+        new ActionRowBuilder().addComponents(reason)
+    );
+
+    return modal;
+
+};
