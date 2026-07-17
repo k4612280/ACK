917ac11aa14647808e643b461a75b509d88f4370
k4612280 <k4612280@gmail.com>
2026-07-17 21:09:27 +0300

    Update prisoners.js

--- src/database/prisoners.js
+++ src/database/prisoners.js
@@ -5,35 +5,92 @@ module.exports = {
     create(data) {
 
         return db.prepare(`
-        INSERT INTO prisoners
-        (guild_id,user_id,officer_id,reason,sentence)
-        VALUES (?,?,?,?,?)
-        `).run(
-            data.guild_id,
-            data.user_id,
-            data.officer_id,
-            data.reason,
-            data.sentence
-        );
+            INSERT INTO prisoners (
+
+                guild_id,
+                user_id,
+                username,
+                prison_id,
+                previous_roles,
+                nickname,
+                reason,
+                case_number,
+                officer_id,
+                officer_name,
+                entry_time,
+                release_time,
+                prison_duration
+
+            )
+
+            VALUES (
+
+                @guild_id,
+                @user_id,
+                @username,
+                @prison_id,
+                @previous_roles,
+                @nickname,
+                @reason,
+                @case_number,
+                @officer_id,
+                @officer_name,
+                @entry_time,
+                @release_time,
+                @prison_duration
+
+            )
+        `).run(data);
 
     },
 
     get(userId) {
 
-        return db.prepare(
-            "SELECT * FROM prisoners WHERE user_id = ? AND status='jailed'"
-        ).get(userId);
+        return db.prepare(`
+            SELECT *
+            FROM prisoners
+            WHERE user_id = ?
+            AND status = 'imprisoned'
+        `).get(userId);
+
+    },
+
+    getCase(caseNumber) {
+
+        return db.prepare(`
+            SELECT *
+            FROM prisoners
+            WHERE case_number = ?
+        `).get(caseNumber);
 
     },
 
     release(userId) {
 
         return db.prepare(`
-        UPDATE prisoners
-        SET status='released',
-        released_at=CURRENT_TIMESTAMP
-        WHERE user_id=?
+            UPDATE prisoners
+            SET status = 'released'
+            WHERE user_id = ?
         `).run(userId);
+
+    },
+
+    remove(userId) {
+
+        return db.prepare(`
+            DELETE FROM prisoners
+            WHERE user_id = ?
+        `).run(userId);
+
+    },
+
+    all() {
+
+        return db.prepare(`
+            SELECT *
+            FROM prisoners
+            ORDER BY entry_time DESC
+        `).all();
 
     }
 
