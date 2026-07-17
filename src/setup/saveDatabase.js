02012a9589ff75355edc2772cfed76bbd8849673
k4612280 <k4612280@gmail.com>
2026-07-17 18:38:43 +0300

    Update saveDatabase.js

--- src/setup/saveDatabase.js
+++ src/setup/saveDatabase.js
@@ -1,1 +1,15 @@
-ي
+const settings = require("../database/settings");
+
+module.exports = (guildId, data) => {
+
+    for (const key in data) {
+
+        settings.update(
+            key,
+            data[key],
+            guildId
+        );
+
+    }
+
+};
