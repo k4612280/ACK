28eb8f48abd1ce3cabafea95389feffb8ac81f2c
k4612280 <k4612280@gmail.com>
2026-07-17 18:46:33 +0300

    Update officersMenu.js

--- src/menus/officersMenu.js
+++ src/menus/officersMenu.js
@@ -1,1 +1,46 @@
-س
+const {
+    ActionRowBuilder,
+    StringSelectMenuBuilder
+} = require("discord.js");
+
+module.exports = () => {
+
+    return new ActionRowBuilder().addComponents(
+
+        new StringSelectMenuBuilder()
+
+            .setCustomId("officer_menu")
+
+            .setPlaceholder("اختر العملية")
+
+            .addOptions(
+
+                {
+                    label: "سجن",
+                    value: "prison",
+                    emoji: "🔒"
+                },
+
+                {
+                    label: "إطلاق سراح",
+                    value: "release",
+                    emoji: "🔓"
+                },
+
+                {
+                    label: "بصمة",
+                    value: "fingerprint",
+                    emoji: "🪪"
+                },
+
+                {
+                    label: "المخالفات",
+                    value: "violations",
+                    emoji: "📑"
+                }
+
+            )
+
+    );
+
+};
