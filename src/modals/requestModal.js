c05cc7809a2ce7355bcad576bdaa63c549d579a0
k4612280 <k4612280@gmail.com>
2026-07-17 18:57:50 +0300

    Update requestModal.js

--- src/modals/requestModal.js
+++ src/modals/requestModal.js
@@ -1,1 +1,26 @@
-ط
+const {
+    ModalBuilder,
+    ActionRowBuilder,
+    TextInputBuilder,
+    TextInputStyle
+} = require("discord.js");
+
+module.exports = () => {
+
+    const modal = new ModalBuilder()
+        .setCustomId("request_modal")
+        .setTitle("طلب سجين");
+
+    const request = new TextInputBuilder()
+        .setCustomId("request")
+        .setLabel("اكتب الطلب")
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(true);
+
+    modal.addComponents(
+        new ActionRowBuilder().addComponents(request)
+    );
+
+    return modal;
+
+};
