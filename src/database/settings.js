f314e226621b6677988725cf96e063d963e266b9
k4612280 <k4612280@gmail.com>
2026-07-17 18:42:06 +0300

    Update settings.js

--- src/database/settings.js
+++ src/database/settings.js
@@ -4,19 +4,37 @@ module.exports = {
 
     get(guildId) {
 
-        return db.prepare("SELECT * FROM settings WHERE guild_id = ?").get(guildId);
+        return db.prepare(
+            "SELECT * FROM settings WHERE guild_id=?"
+        ).get(guildId);
 
     },
 
     create(guildId) {
 
-        return db.prepare("INSERT OR IGNORE INTO settings (guild_id) VALUES (?)").run(guildId);
+        return db.prepare(
+            "INSERT OR IGNORE INTO settings(guild_id) VALUES(?)"
+        ).run(guildId);
 
     },
 
     update(column, value, guildId) {
 
-        return db.prepare(`UPDATE settings SET ${column} = ? WHERE guild_id = ?`).run(value, guildId);
+        const allowed = [
+            "prison_role_id",
+            "officer_role_id",
+            "media_role_id",
+            "admin_role_id",
+            "logs_channel_id",
+            "prison_channel_id",
+            "media_channel_id"
+        ];
+
+        if (!allowed.includes(column)) return;
+
+        db.prepare(
+            `UPDATE settings SET ${column}=? WHERE guild_id=?`
+        ).run(value, guildId);
 
     }
 
