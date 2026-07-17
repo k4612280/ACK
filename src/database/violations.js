c6a6d7333430c3ac3397a762cd8b9743bdfe1b3a
k4612280 <k4612280@gmail.com>
2026-07-17 21:09:45 +0300

    Update violations.js

--- src/database/violations.js
+++ src/database/violations.js
@@ -1,1 +1,61 @@
-شوق
+const db = require("./init");
+
+module.exports = {
+
+    create(data) {
+
+        return db.prepare(`
+            INSERT INTO violations (
+
+                prisoner_id,
+                guild_id,
+                reason,
+                officer_id,
+                officer_name
+
+            )
+
+            VALUES (
+
+                @prisoner_id,
+                @guild_id,
+                @reason,
+                @officer_id,
+                @officer_name
+
+            )
+        `).run(data);
+
+    },
+
+    get(prisonerId) {
+
+        return db.prepare(`
+            SELECT *
+            FROM violations
+            WHERE prisoner_id = ?
+            ORDER BY id DESC
+        `).all(prisonerId);
+
+    },
+
+    delete(id) {
+
+        return db.prepare(`
+            DELETE FROM violations
+            WHERE id = ?
+        `).run(id);
+
+    },
+
+    all() {
+
+        return db.prepare(`
+            SELECT *
+            FROM violations
+            ORDER BY id DESC
+        `).all();
+
+    }
+
+};
