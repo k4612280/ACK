de5edfd0cfe14c7e566ebc7527fb4c0f96c717c6
k4612280 <k4612280@gmail.com>
2026-07-17 18:46:55 +0300

    Update officers.js

--- src/commands/officers.js
+++ src/commands/officers.js
@@ -1,1 +1,36 @@
-s
+const {
+    SlashCommandBuilder
+} = require("discord.js");
+
+const officerPanel = require("../embeds/officerPanel");
+const officerMenu = require("../menus/officerMenu");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+
+        .setName("officers")
+
+        .setDescription("إرسال لوحة الضباط"),
+
+    async execute(interaction) {
+
+        await interaction.reply({
+
+            embeds: [
+
+                officerPanel()
+
+            ],
+
+            components: [
+
+                officerMenu()
+
+            ]
+
+        });
+
+    }
+
+};
